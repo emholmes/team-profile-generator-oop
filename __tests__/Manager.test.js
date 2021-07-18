ae442b5cc453dd2f81165d1fc0a861106bf6ae26
Erin Holmes <e.holmes09@yahoo.com>
2021-07-17 20:00:40 -0500

    update Manager class and test to include getOfficeNumber method

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -6,8 +6,13 @@ test("create a manager object", () => {
   expect(manager.name).toBe("Liz");
   expect(manager.id).toEqual(expect.any(Number));
   expect(manager.email).toBe("liz@email.com");
-  expect(manager.officeNumber).toBe("555-555-5555");
 });
+
+test("get manager's office number", () => {
+  const manager = new Manager("Liz", 1, "liz@email.com", "555-555-5555");
+
+  expect(manager.officeNumber).toBe("555-555-5555");
+})
 
 test("get manager's role", () => {
   const manager = new Manager("Liz");
