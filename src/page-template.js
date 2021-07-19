899aebf281135b19617c6c5d28d364902fece9ca
Erin Holmes <e.holmes09@yahoo.com>
2021-07-18 20:35:28 -0500

    remove role variable from generating card fns, not needed

--- src/page-template.js
+++ src/page-template.js
@@ -1,11 +1,11 @@
-const generateUniqueProp = (employee, role) => {
-  if (role === "Manager") {
+const generateUniqueProp = (employee) => {
+  if (employee.getRole() === "Manager") {
     return `<li>Office number: ${employee.getOfficeNumber().trim()}</li>`;
   }
-  if (role === "Engineer") {
+  if (employee.getRole() === "Engineer") {
     return `<li>GitHub: <a href="https://github.com/${employee.getGitHub().trim()}" class="link">${employee.getGitHub().trim()}</a></li>`;
   }
-  if (role === "Intern") {
+  if (employee.getRole() === "Intern") {
     return `<li>School: ${employee.getSchool().trim()}</li>`
   }
 }
@@ -14,17 +14,16 @@ const generateTeamCards = (teamArray) => {
   return `
     ${teamArray
       .map(employee => {
-        let role = employee.getRole();
         return `
           <section class="card">
             <div class="card-header">
               <h2>${employee.getName().trim()}</h2>
-              <p>${role}</p>
+              <p>${employee.getRole()}</p>
             </div>
             <ul class="card-body">
               <li>ID: ${employee.getId().trim()}</li>
               <li>Email: <a href="mailto:${employee.getEmail().trim()}" class="link">${employee.getEmail().trim()}</a></li>
-              ${generateUniqueProp(employee, role)}
+              ${generateUniqueProp(employee)}
             </ul>
           </section>
         `;
